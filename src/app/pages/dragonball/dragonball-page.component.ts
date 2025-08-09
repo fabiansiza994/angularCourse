d8207a77208e2e2183ba9ed865ec89a06b527326
fabiansiza994 <fabiansiza994@gmail.com>
2025-08-08 22:12:15 -0500

    agregar datos al array

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -13,11 +13,42 @@ interface Character {
   styleUrl: './dragonball-page.component.css'
 })
 export class DragonballComponent {
+
+  name = signal('Dragon Ball');
+  ability = signal('Kamehameha');
+  power = signal(9000);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', ability: 'Kamehameha', power: 9000 },
     { id: 2, name: 'Vegeta', ability: 'Final Flash', power: 8500 },
     { id: 3, name: 'Gohan', ability: 'Masenko', power: 8000 },
     { id: 4, name: 'Piccolo', ability: 'Special Beam Cannon', power: 7000 },
     { id: 5, name: 'Frieza', ability: 'Death Ball', power: 9500 },
+    { id: 6, name: 'Cell', ability: 'Solar Kamehameha', power: 9000 },
+    { id: 7, name: 'Majin Buu', ability: 'Planet Burst', power: 10000 },
+    { id: 8, name: 'Trunks', ability: 'Burning Attack', power: 7500 },
+    { id: 9, name: 'Krillin', ability: 'Destructo Disc', power: 6000 },
+    { id: 10, name: 'Yamcha', ability: 'Wolf Fang Fist', power: 500 }
   ]);
+
+  addCharacter() {
+    if(!this.name() || !this.ability() || !this.power()) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      ability: this.ability(),
+      power: this.power()
+    };
+    this.characters.update(chars => [...chars, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.ability.set('');
+    this.power.set(0);
+  }
 }
